bd4df8d03fe416e540ee1944569e87f7e8faa577
dmlakcin <damla.akcin11@gmail.com>
2022-10-10 17:29:15 +0300

    random

--- src/scripts/utils/upload.js
+++ src/scripts/utils/upload.js
@@ -3,7 +3,7 @@ import multer from 'multer'
 const storage = multer.diskStorage({
   destination: './uploads',
   filename: (req, file, cb) => {
-    cb(null, file.originalname)
+    cb(null, `${+Date.now()}-${file.originalname}`)
   },
 })
 
