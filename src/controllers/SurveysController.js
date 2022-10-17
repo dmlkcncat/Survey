f284525c48e5c8508b4795faa315d09aea9c769d
eminakcil <eminakcil@outlook.com>
2022-10-17 14:54:35 +0300

    service changes

--- src/controllers/SurveysController.js
+++ src/controllers/SurveysController.js
@@ -5,4 +5,11 @@ export default class SurveysController extends BaseController {
   constructor() {
     super(new SurveysService())
   }
+
+  getById = (req, res) => {
+    this.service
+      .get({ _id: req.params.id })
+      .then((response) => res.status(200).send(response))
+      .catch((e) => res.status(500).send(e))
+  }
 }
